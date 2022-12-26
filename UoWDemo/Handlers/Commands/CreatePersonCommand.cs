81417e8bdcff28162dd9e0ce76f0d386d85165f2
Maxim Stanciu <maxim.stanciu@ucms.ro>
2022-12-26 14:27:26 +0000

    Removed SQLite

---
+++ UoWDemo/Handlers/Commands/CreatePersonCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace UoWDemo.Handlers.Commands
+{
+    //return Result.Created;
+    //https://www.youtube.com/watch?v=v19arLqQkP8&ab_channel=AmichaiMantinband upsert
+    public class CreatePersonCommand
+    {
+    }
+}
