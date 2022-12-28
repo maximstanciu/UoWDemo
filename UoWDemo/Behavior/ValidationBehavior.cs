5b7ba93c750a6c99121f284052ce3f237f52ef68
Maxim Stanciu <maxim.stanciu@ucms.ro>
2022-12-28 14:57:56 +0000

    Validation Behaviour improvement

--- UoWDemo/Behavior/ValidationBehavior.cs
+++ UoWDemo/Behavior/ValidationBehavior.cs
@@ -1,6 +1,8 @@
 ﻿using ErrorOr;
 using FluentValidation;
+using FluentValidation.Results;
 using MediatR;
+using System.Reflection;
 
 namespace UoWDemo.Behavior
 {
@@ -17,19 +19,33 @@ namespace UoWDemo.Behavior
 
         public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
         {
-            if (_validator is null)
+            if (_validator == null)
                 return await next();
 
             var validationResult = await _validator.ValidateAsync(request, cancellationToken);
+
             if (validationResult.IsValid)
                 return await next();
 
-            var errors = validationResult.Errors
-                .ConvertAll(e => Error.Validation(e.PropertyName, e.ErrorMessage))
-                .ToList();
+            return TryCreateResponseFromErrors(validationResult.Errors, out var response)
+                ? response
+                : throw new ValidationException(validationResult.Errors);
+        }
 
-            //NOTE: Its danger, but! Check git!
-            return (dynamic)errors;
+        private static bool TryCreateResponseFromErrors(List<ValidationFailure> validationFailures, out TResponse response)
+        {
+            List<Error> errors = validationFailures.ConvertAll(x => Error.Validation(
+                code: x.PropertyName,
+                description: x.ErrorMessage));
+
+            response = (TResponse?)typeof(TResponse)
+                .GetMethod(
+                    name: nameof(ErrorOr<object>.From),
+                    bindingAttr: BindingFlags.Static | BindingFlags.Public,
+                    types: new[] { typeof(List<Error>) })?
+                .Invoke(null, new[] { errors })!;
+
+            return response is not null;
         }
     }
 }
